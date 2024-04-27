7206b21275b53bb556951dd5b3cbb7d3ae752776
Cialyni <kvacheg1002@gmail.com>
2024-04-27 17:09:00 +0300

    add type: ignore[override] to minor mypy errors

--- src/Homeworks/deramida.py
+++ src/Homeworks/deramida.py
@@ -55,7 +55,9 @@ class NodeComparator(Generic[K]):
     def __gt__(self, other: "Node") -> bool:
         return not (self < other)
 
-    def __eq__(self, other: "Node") -> bool:
+    def __eq__(self, other: "Node") -> bool:    # type: ignore[override]
+        if not isinstance(other, NodeComparator):
+            return NotImplemented
         if self is None and other is None:
             return True
         if self is None or other is None:
@@ -86,7 +88,7 @@ class Deramida(MutableMapping, PrinterMixin, Generic[K, V]):
             raise KeyError
         return needed.value
 
-    def __contains__(self, key: K) -> bool:
+    def __contains__(self, key: K) -> bool: # type: ignore[override]
         try:
             return True if self[key] is not None else False
         except KeyError:
