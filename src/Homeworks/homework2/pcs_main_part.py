26176713b316fdd656238da34792c3c680407d86
Cialyni <kvacheg1002@gmail.com>
2024-05-08 18:16:09 +0300

    change imports

--- src/Homeworks/homework2/pcs_main_part.py
+++ src/Homeworks/homework2/pcs_main_part.py
@@ -1,4 +1,4 @@
-from src.Homeworks.homework2.perfomans_command_storage import *
+from src.Homeworks.homework2.perfomans_command_storage import Action, PerformedCommandStorage
 from src.Homeworks.registry import Registry
 
 INFO = """\n(First word - command; i, j - indexes)\n
