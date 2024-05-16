bc7c12c98d17ac17909ecc4184578569afe09a44
Cialyni <kvacheg1002@gmail.com>
2024-05-16 20:45:58 +0300

    task 1.1

---
+++ src/Retest/Retest_1/Univer.py
@@ -0,0 +1,105 @@
+import pprint
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass
+class TeacherCourses:
+    course_name: str
+    students_names: List[str]
+
+
+@dataclass
+class StudentCourses:
+    course_name: str
+    teacher_name: str
+    attendance: float
+    scores: List[float]
+
+
+class Teacher:
+    def __init__(self, name: str, courses: List[TeacherCourses]) -> None:
+        self.name = name
+        self.courses = courses
+
+
+class Student:
+    def __init__(self, name: str, courses: List[StudentCourses]) -> None:
+        self.name = name
+        self.courses = courses
+
+    def find_middle_score(self, course_name: str) -> float:
+        for course in self.courses:
+            if course.course_name == course_name:
+                return sum(course.scores) / len(course.scores)
+        raise ValueError("This student does not have such a subject")
+
+    def find_attendance(self, course_name: str) -> float:
+        for course in self.courses:
+            if course.course_name == course_name:
+                return course.attendance
+        raise ValueError("This student does not have such a subject")
+
+
+class University:
+    def __init__(self, students: List[Student], teachers: List[Teacher]) -> None:
+        self.students: List[Student] = students
+        self.teachers: List[Teacher] = teachers
+
+    def find_student_middle_score(self, student_name: str, course_name: str) -> Optional[float]:
+        try:
+            for student in self.students:
+                if student.name == student_name:
+                    return student.find_middle_score(course_name)
+        except ValueError as error:
+            print(error)
+        else:
+            print("This student does not exist")
+
+    def find_student_attendance(self, student_name: str, course_name: str) -> Optional[float]:
+        try:
+            for student in self.students:
+                if student.name == student_name:
+                    return student.find_attendance(course_name)
+        except ValueError as error:
+            print(error)
+        else:
+            print("This student does not exist")
+
+    def find_teacher_workload(self, teacher_name: str) -> Optional[int]:
+        for teacher in self.teachers:
+            if teacher.name == teacher_name:
+                return len(teacher.courses)
+        print("This teacher does not exist")
+
+    def find_teacher_courses(self, teacher_name: str) -> List[str]:
+        courses = []
+        for teacher in self.teachers:
+            if teacher.name == teacher_name:
+                for course in teacher.courses:
+                    courses.append(course.course_name)
+        return courses
+
+    def add_student(self, student_name: str) -> None:
+        student = Student(student_name, [])
+        self.students.append(student)
+
+    def add_student_course(
+        self, student_name: str, course_name: str, teacher_name: str, attendance: float, scores: List[float]
+    ) -> None:
+        course = StudentCourses(course_name, teacher_name, attendance, scores)
+        for i, student in enumerate(self.students):
+            if student.name == student_name:
+                self.students[i].courses.append(course)
+                break
+
+    def add_teacher(self, teacher_name: str) -> None:
+        teacher = Teacher(teacher_name, [])
+        self.teachers.append(teacher)
+
+    def add_teacher_course(self, teacher_name: str, course_name: str, students: List[str]) -> None:
+        course = TeacherCourses(course_name, students)
+        for i, teacher in enumerate(self.teachers):
+            if teacher.name == teacher_name:
+                self.teachers[i].courses.append(course)
+                break
---
+++ tests/Retest/Retest_1/test_Univer.py
@@ -0,0 +1,126 @@
+import pytest
+
+from src.Retest.Retest_1.Univer import *
+
+UNIVER = University(
+    [
+        Student(
+            "Ilia",
+            [
+                StudentCourses(
+                    course_name="Math", teacher_name="Genadi Sergeevich", attendance=0.56, scores=[1, 5, 3, 4]
+                ),
+                StudentCourses(
+                    course_name="Topology", teacher_name="Ivan Ivanovitch", attendance=0.2, scores=[5, 5, 5, 4]
+                ),
+                StudentCourses(
+                    course_name="Algebra", teacher_name="Maksim Alexeevitch", attendance=0.1, scores=[1, 1, 3, 2]
+                ),
+                StudentCourses(
+                    course_name="Informatica", teacher_name="Maksim Alexeevitch", attendance=0.44, scores=[1, 5, 1, 2]
+                ),
+            ],
+        ),
+        Student(
+            "Masha",
+            [
+                StudentCourses(
+                    course_name="Topology", teacher_name="Genadi Sergeevich", attendance=0.97, scores=[5, 5, 5, 4]
+                ),
+                StudentCourses(
+                    course_name="Algebra", teacher_name="Maksim Alexeevitch", attendance=0.87, scores=[1, 4, 4, 4]
+                ),
+            ],
+        ),
+        Student(
+            "Anton",
+            [
+                StudentCourses(
+                    course_name="Algebra", teacher_name="Maksim Alexeevitch", attendance=0.65, scores=[1, 2, 3, 4]
+                ),
+                StudentCourses(
+                    course_name="Topology", teacher_name="Genadi Sergeevich", attendance=0.45, scores=[5, 5, 1, 4]
+                ),
+                StudentCourses(
+                    course_name="Informatica", teacher_name="Maksim Alexeevitch", attendance=0.76, scores=[1, 3, 3, 4]
+                ),
+            ],
+        ),
+    ],
+    [
+        Teacher(
+            "Maksim Alexeevitch",
+            [
+                TeacherCourses(course_name="Algebra", students_names=["Masha, Anton, Ilia"]),
+                TeacherCourses(course_name="Informatica", students_names=["Anton, Ilia"]),
+            ],
+        ),
+        Teacher(
+            "Genadi Sergeevich",
+            [
+                TeacherCourses(course_name="Math", students_names=["Ilia"]),
+                TeacherCourses(course_name="Topology", students_names=["Anton, Masha"]),
+            ],
+        ),
+        Teacher("Ivan Ivanovitch", [TeacherCourses(course_name="Topology", students_names=["Ilia"])]),
+    ],
+)
+
+
+class TestStudent:
+    @pytest.mark.parametrize(
+        "student_name, course_name, expected",
+        (
+            ("Masha", "Math", None),
+            ("Masha", "Topology", 4.75),
+            ("Anton", "Topology", 3.75),
+            ("Ilia", "Algebra", 1.75),
+        ),
+    )
+    def test_find_middle_score(self, student_name, course_name, expected):
+        assert UNIVER.find_student_middle_score(student_name, course_name) == expected
+
+    @pytest.mark.parametrize(
+        "student_name, course_name, expected",
+        (
+            ("Masha", "Math", None),
+            ("Masha", "Topology", 0.97),
+            ("Anton", "Topology", 0.45),
+            ("Ilia", "Algebra", 0.1),
+        ),
+    )
+    def test_find_attendance(self, student_name, course_name, expected):
+        assert UNIVER.find_student_attendance(student_name, course_name) == expected
+
+
+class TestTeacher:
+    @pytest.mark.parametrize(
+        "teacher_name,  expected",
+        (
+            ("Genadi Sergeevich", ["Math", "Topology"]),
+            ("Maksim Alexeevitch", ["Algebra", "Informatica"]),
+            ("Ivan Ivanovitch", ["Topology"]),
+            ("adfasf", []),
+        ),
+    )
+    def test_find_teacher_courses(self, teacher_name, expected):
+        assert UNIVER.find_teacher_courses(teacher_name) == expected
+
+    @pytest.mark.parametrize(
+        "teacher_name, , expected",
+        (("Genadi Sergeevich", 2), ("Maksim Alexeevitch", 2), ("Ivan Ivanovitch", 1), ("asadfsadf", None)),
+    )
+    def test_find_teacher_workload(self, teacher_name, expected):
+        assert UNIVER.find_teacher_workload(teacher_name) == expected
+
+
+class TestAdd:
+    def test_add_student(self):
+        UNIVER.add_student("Kriss")
+        UNIVER.add_student_course("Kriss", "a", "b", 0, [1])
+        assert UNIVER.find_student_attendance("Kriss", "a") == 0
+
+    def test_add_teacher(self):
+        UNIVER.add_teacher("Mihail")
+        UNIVER.add_teacher_course("Mihail", "qq", ["aboba"])
+        assert UNIVER.find_teacher_courses("Mihail") == ["qq"]
