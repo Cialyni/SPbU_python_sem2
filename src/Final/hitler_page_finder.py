7c3cb1dca176c90e2d473e70e7bccadc7ef4dc87
Cialyni <kvacheg1002@gmail.com>
2024-05-24 00:33:06 +0300

    black + isort

--- src/Final/hitler_page_finder.py
+++ src/Final/hitler_page_finder.py
@@ -7,13 +7,18 @@ from typing import List
 import requests
 from bs4 import BeautifulSoup
 
+needed_to_find = "https://en.wikipedia.org/wiki/Adolf_Hitler"
 
-needed_to_find = 'https://en.wikipedia.org/wiki/Adolf_Hitler'
 
 def bfs_with_multyprocces(start_urls: List[str], max_depth: int, workers):
     with ProcessPoolExecutor(max_workers=workers) as executor:
         step = ceil(len(start_urls) / workers)
-        fs = [executor.submit(bfs_hitler_search, start_urls[i * step: min((i+1)*step, len(start_urls))], max_depth, i) for i in range(workers)]
+        fs = [
+            executor.submit(
+                bfs_hitler_search, start_urls[i * step : min((i + 1) * step, len(start_urls))], max_depth, i
+            )
+            for i in range(workers)
+        ]
         minimal = [""] * 99999
         for f in as_completed(fs):
             result = f.result()
@@ -23,8 +28,9 @@ def bfs_with_multyprocces(start_urls: List[str], max_depth: int, workers):
                 minimal = result
         return minimal
 
+
 def bfs_hitler_search(start_urls: List[str], max_depth: int, pack_num: int):
-    print('Checking process number:', pack_num)
+    print("Checking process number:", pack_num)
     que = Queue()
     for url in start_urls:
         que.put([url])
@@ -43,9 +49,9 @@ def bfs_hitler_search(start_urls: List[str], max_depth: int, pack_num: int):
 def url_scan(current_url: str) -> List[str]:
     res = []
     reqs = requests.get(current_url)
-    soup = BeautifulSoup(reqs.text, 'html.parser')
-    for link in soup.find_all('a'):
-        wiki_suff = link.get('href')
+    soup = BeautifulSoup(reqs.text, "html.parser")
+    for link in soup.find_all("a"):
+        wiki_suff = link.get("href")
         if wiki_suff and wiki_suff[0:6] == "/wiki/":
             res.append("https://en.wikipedia.org" + wiki_suff)
     return list(set(res))
